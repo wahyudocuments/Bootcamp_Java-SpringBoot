75b4962950a6794955441661e0c42ce7c1f8f9b6
Wahyu Mulya Utama <wahyumulyautama@gmail.com>
2023-01-25 11:14:57 +0700

    Arraylist

--- Java-1/src/com/sgiasia/javaspringboot/Main.java
+++ Java-1/src/com/sgiasia/javaspringboot/Main.java
@@ -3,31 +3,50 @@ package com.sgiasia.javaspringboot;
 import com.sgiasia.javaspringboot.model.Admin;
 import com.sgiasia.javaspringboot.model.User;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        User operator1 = new User("Budi", 23);
-//        operator1.cetakNama();
-//        operator1.cetakUmur();
-//
-//        User operator2 = new User("Badu");
-//        operator2.cetakNama();
-//
-//        operator1.setUserName("Wahyu");
-//        operator1.getUserName();
+        String[] users = {"Ani", "Badu", "Budi"};
+        int userCount = users.length;
+        ArrayList<String> cars = new ArrayList<String>();
+        /** Add */
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
 
-        /** use Scanner */
-        Scanner myObj = new Scanner(System.in);
-        System.out.print("Enter Username: ");
-        String username = myObj.nextLine();
-        System.out.println("Username is " + username);
+        /** Acces item  (get) */
+        cars.get(0);
+        System.out.println("Acces : "  + cars.get(0));
 
-        /** Inheritance */
+        /** Change item (set) */
+        cars.set(0, "Opel");
+        System.out.println("Change : " + cars.set(0,"Toyota"));
+        System.out.println(cars);
 
-        Admin admin1 = new Admin();
-        admin1.setPermission("Oke");
-        admin1.getPermission();
+        /** Remove item (remove) */
+        cars.remove(0);
+        cars.clear();
+
+        /** Size (size) */
+        cars.size();
+
+        /** Loop Through  (for) */
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println(cars.get(i));
+        }
+        // for each
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+
+
+
+
 
 
 
